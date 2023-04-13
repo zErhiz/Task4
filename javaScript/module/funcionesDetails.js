deccb859cc9c1235cbc529f440a79161a2d53bd2
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-13 16:44:53 -0300

    quitno commit

--- javaScript/module/funcionesDetails.js
+++ javaScript/module/funcionesDetails.js
@@ -1,16 +1,23 @@
 export function pintarDescripcionDeEventos(eventoAmazing, detallesCarta) {
-    let eventosImpresos = ""
-    eventosImpresos = ` <div class="letras"><h2 class="titulo22">${eventoAmazing.name}</h2>
-    <h4 class="titulo33">Description:${eventoAmazing.description}</h4>
-    <h4>Date: ${eventoAmazing.date}</h4>
-    <h4>Category: ${eventoAmazing.category}</h4>
-    <h4>Place: ${eventoAmazing.place}</h4>
-    <h4>Capacity: ${eventoAmazing.capacity}</h4>
-    <h4>Assistance: ${eventoAmazing.assistance}</h4>
-    <h4>Price: ${eventoAmazing.price}</h4> </div>`
-  
-    detallesCarta.innerHTML = eventosImpresos
+  let eventosImpresos = ""
+  eventosImpresos = ` <div class="letras"><h2 class="titulo22">${eventoAmazing.name}</h2>
+  <h4 class="titulo33">Description:${eventoAmazing.description}</h4>
+  <h4>Date: ${eventoAmazing.date}</h4>
+  <h4>Category: ${eventoAmazing.category}</h4>
+  <h4>Place: ${eventoAmazing.place}</h4>
+  <h4>Capacity: ${eventoAmazing.capacity}</h4>
+  `;
+
+  if (eventoAmazing.assistance !== undefined) {
+    eventosImpresos += `<h4>Assistance: ${eventoAmazing.assistance}</h4>`;
+  } else {
+    eventosImpresos += `<h4>Estimate: ${eventoAmazing.estimate}</h4>`;
   }
+
+  eventosImpresos += `<h4>Price: ${eventoAmazing.price}</h4> </div>`
+
+  detallesCarta.innerHTML = eventosImpresos;
+}
 
 export function pintarImagenEventos(eventoAmazing,contenedorImg) {
     let eventoImagenImpreso = "";
